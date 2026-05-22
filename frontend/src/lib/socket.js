348861c121900700305ab4689d3d38729978e7d4
mr28Verma <2006sakshamchd@gmail.com>
2026-05-22 21:14:24 +0530

    add gitignore

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,3 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+const URL =
+  typeof window !== "undefined" &&
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000"
+    : "https://watchtogether-backend-w26b.onrender.com";
+
+export const socket = io(URL);
